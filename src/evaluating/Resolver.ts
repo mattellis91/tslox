21d090df02dab26b8c14c639bb95ceca7d9eba17
mattellis91 <mathew.ellis2@griffithuni.edu.au>
2023-08-22 12:53:35 +1000

    added resolver visitor methods

---
+++ src/evaluating/Resolver.ts
@@ -0,0 +1,185 @@
+import { Token } from "../lexing/Token";
+import { AssignmentExpression } from "../parsing/AssignmentExpression";
+import { BinaryExpression } from "../parsing/BinaryExpression";
+import { BlockStatement } from "../parsing/BlockStatement";
+import { CallExpression } from "../parsing/CallExpression";
+import { Expression, ExpressionVisitor } from "../parsing/Expression";
+import { ExpressionStatement } from "../parsing/ExpressionStatement";
+import { FunctionStatement } from "../parsing/FunctionStatement";
+import { GroupingExpression } from "../parsing/GroupingExpression";
+import { IfStatement } from "../parsing/IfStatement";
+import { LiteralExpression } from "../parsing/LiteralExpression";
+import { LogicalExpression } from "../parsing/LogicalExpression";
+import { PrintStatement } from "../parsing/PrintStatement";
+import { ReturnStatement } from "../parsing/ReturnStatement";
+import { Statement, StatementVisitor } from "../parsing/Statement";
+import { UnaryExpression } from "../parsing/UnaryExpression";
+import { VariableExpression } from "../parsing/VariableExpression";
+import { VariableStatement } from "../parsing/VariableStatement";
+import { WhileStatement } from "../parsing/WhileStatement";
+import { Interpreter } from "./Interperter";
+
+export class Resolver implements ExpressionVisitor, StatementVisitor {
+    readonly interpreter: Interpreter;
+    readonly scopes: Map<string, boolean>[] = [];
+
+    constructor(interpreter: Interpreter) {
+        this.interpreter = interpreter;
+    }
+
+    resolve(statements: Statement[]) {
+        for(const statement of statements) {
+            this.resolveStatement(statement);
+        }
+    }
+
+    resolveStatement(statement: Statement) {
+        statement.accept(this);
+    }
+
+    resolveExpression(expression: Expression) {
+        expression.accept(this);
+    }
+
+    resolveFunction(fs: FunctionStatement) {
+        this.beginScope();
+        for(const param of fs.params) {
+            this.declare(param);
+            this.define(param);
+        }
+        this.resolve(fs.body);
+        this.endScope();
+    }
+
+    resolveLocal(expression: Expression, name: Token) {
+        for(let i = this.scopes.length - 1; i >= 0; i--) {
+            if(this.scopes[i].has(name.lexeme)) {
+                this.interpreter.resolveExpression(expression, this.scopes.length - 1 - i);
+                return;
+            }
+        }
+    }
+    
+    visitForBlockStatement(bs: BlockStatement) {
+        this.beginScope();
+        this.resolve(bs.statements);
+        this.endScope();
+        return null;
+    }
+
+    visitForAssignmentExpression(ae: AssignmentExpression) {
+        this.resolveExpression(ae.value);
+        this.resolveLocal(ae, ae.name);
+        return null;
+    }
+
+    visitForFunctionStatement(fs: FunctionStatement) {
+        this.declare(fs.name);
+        this.define(fs.name);
+        this.resolveFunction(fs);
+        return null;
+    }
+
+    visitForVariableStatement(vs: VariableStatement) {
+        this.declare(vs.name);
+        if(vs.initializer !== null) {
+            this.resolveExpression(vs.initializer);
+        }
+        this.define(vs.name);
+        return null;
+    }
+
+    visitForVariableExpression(ve: VariableExpression) {
+        if(this.scopes.length > 0 && this.scopes[this.scopes.length - 1].get(ve.name.lexeme) === false) {
+            throw new Error("Cannot read local variable in its own initializer.");
+        }
+        this.resolveLocal(ve, ve.name);
+        return null;
+    }
+
+    visitForExpressionStatement(es: ExpressionStatement) {
+        this.resolveExpression(es.expression);
+        return null;
+    }
+
+    visitForIfStatement(is: IfStatement) {
+        this.resolveExpression(is.condition);
+        this.resolveStatement(is.thenBranch);
+        if(is.elseBranch !== null) {
+            this.resolveStatement(is.elseBranch);
+        }
+        return null;
+    }
+
+    visitForPrintStatement(ps: PrintStatement) {
+        this.resolveExpression(ps.expression);
+        return null;
+    }
+
+    visitForReturnStatement(ps: ReturnStatement) {
+        if(ps.value !== null) {
+            this.resolveExpression(ps.value);
+        }
+        return null;
+    }
+
+    visitForWhileStatement(ws: WhileStatement) {
+        this.resolveExpression(ws.condition);
+        this.resolveStatement(ws.body);
+        return null;
+    }
+
+    visitForBinaryExpression(be: BinaryExpression) {
+        this.resolveExpression(be.left);
+        this.resolveExpression(be.right);
+        return null;
+    }
+
+    visitForCallExpression(ce: CallExpression) {
+        this.resolveExpression(ce.callee);
+        for(const argument of ce.args) {
+            this.resolveExpression(argument);
+        }
+        return null;
+    }
+
+    visitForGroupingExpression(ge: GroupingExpression) {
+        this.resolveExpression(ge.expression);
+        return null;
+    }
+
+    visitForLiteralExpression(le: LiteralExpression) {
+        return null;
+    }
+
+    visitForLogicalExpression(le: LogicalExpression) {
+        this.resolveExpression(le.left);
+        this.resolveExpression(le.right);
+        return null;
+    }
+
+    visitForUnaryExpression(ue: UnaryExpression) {
+        this.resolveExpression(ue.right);
+        return null;
+    }
+
+    private declare(name: Token) {
+        if(this.scopes.length === 0) return;
+        const scope = this.scopes[this.scopes.length - 1];
+        scope.set(name.lexeme, false);
+    }
+
+    private define(name: Token) {
+        if(this.scopes.length === 0) return;
+        const scope = this.scopes[this.scopes.length - 1];
+        scope.set(name.lexeme, true);
+    }
+
+    private beginScope() {
+        this.scopes.push(new Map<string, boolean>());
+    }
+
+    private endScope() {
+        this.scopes.pop();
+    }
+}
